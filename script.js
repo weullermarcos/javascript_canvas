e22fa2d6c77e7b3b77527ee6c68b29975644982d
weuller <weullermarcos@hotmail.com>
2021-07-26 21:41:26 -0300

    Desenhando e limpando quadro

--- script.js
+++ script.js
@@ -1,9 +1,43 @@
 
+//Dados iniciais:
 let currentColor = 'black';
+let canDraw = false;
+let screen = document.querySelector('#tela');
+let ctx = screen.getContext('2d');
 
+let mouseX = 0;
+let mouseY =0;
+
+//Eventos:
 document.querySelectorAll('.colorArea .color').forEach(item => {
     item.addEventListener('click', colorClickEvent);
 });
+
+screen.addEventListener('mousedown', mouseDownEvent);
+screen.addEventListener('mousemove', mouseMoveEvent);
+screen.addEventListener('mouseup'  , mouseUpEvent);
+document.querySelector('.clear').addEventListener('click', clearScreen);
+
+//Funções:
+
+function mouseDownEvent(e){
+    canDraw = true;
+
+    mouseX = e.pageX - screen.offsetLeft;
+    mouseY = e.pageY - screen.offsetTop;
+}
+
+function mouseMoveEvent(e){
+
+    if(canDraw){
+
+        draw(e.pageX, e.pageY);
+    }
+}
+
+function mouseUpEvent(){
+    canDraw = false;
+}
 
 function colorClickEvent(e){
 
@@ -16,7 +50,29 @@ function colorClickEvent(e){
 
     //adicionando ativo a nova cor selecionada
     e.target.classList.add('active');
+}
 
+function draw(x, y){
 
+    let pointX = x - screen.offsetLeft;
+    let poinY  = y - screen.offsetTop;
 
+    ctx.beginPath(); //inicia o processo de desenho
+    ctx.lineWidth = 5; //5px de largura para a linha
+    ctx.lineJoin = "round"; //linha com bordas arredondadas
+    ctx.moveTo(mouseX, mouseY); //começa a desenhar
+    ctx.lineTo(pointX,poinY); //define até onde a linha vai
+    ctx.closePath();
+    ctx.strokeStyle = currentColor; //define a cor da linha
+    ctx.stroke(); //finaliza o desenho
+
+    mouseX = pointX;
+    mouseY = poinY;
+}
+
+//limpa a tela
+function clearScreen(){
+
+    ctx.setTransform(1, 0, 0, 1, 0, 0);
+    ctx.clearRect(0,0, ctx.canvas.width, ctx.canvas.height);
 }
